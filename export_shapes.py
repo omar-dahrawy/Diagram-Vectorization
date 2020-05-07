4437028d75a180dcb9b786b92c00f394a563f11a
Omar Al-Dahrawy <omar_dahrawy2000@yahoo.com>
2020-05-07 06:24:12 +0200

    refined code, added another test input image

--- export_shapes.py
+++ export_shapes.py
@@ -21,6 +21,10 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
+cv2.imshow("binary", thresh)
+cv2.waitKey(0)
+
+
 # find contours in the thresholded image
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
@@ -45,7 +49,7 @@ for c in cnts:
     x,y,w,h = cv2.boundingRect(c)
     with open("Data/dimensions.txt", "a") as dimensions:
         dimensions.write("{0},{1},{2},{3}\n".format(x,y,w,h))
-    #cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
     
     # crop shape in image
     crop_img = image[y-10:y+h+10, x-10:x+w+10]
@@ -65,9 +69,16 @@ for c in cnts:
     rect = cv2.minAreaRect(c)
     box = cv2.boxPoints(rect)
     box = np.int0(box)
+    cv2.drawContours(image,[box],0,(0,0,255),2)
+    cv2.circle(image, (box[0][0],box[0][1]), 5, (255,255,255), -1)
+    cv2.circle(image, (box[1][0],box[1][1]), 5, (0,255,0), -1)
     with open("Data/orientations.txt", "a") as dimensions:
         dimensions.write("{0}\n".format(box[0][1]-box[1][1]))
 
+
+# show the output image
+cv2.imshow("image", image)
+cv2.waitKey(0)
 
 # export shapes and create new empty image
 counter = 0
@@ -80,10 +91,6 @@ for shape in exported_images:
 new_image = np.zeros((image.shape[0],image.shape[1],3), np.uint8)
 new_image[:,0:image.shape[1]] = (255,255,255)
 cv2.imwrite(os.path.join("Data/" , 'new-diagram.jpg'), new_image)
-
-# show the output image
-#cv2.imshow("Image", image)
-#cv2.waitKey(0)
 
 # run prediction script on exported images
 import predict
